0c1f47910f88a4fdfdc9da88336585d32f896450
JarretYingling <44506154+JarretYingling@users.noreply.github.com>
2018-12-11 01:04:41 -0500

    visual layout

---
+++ assets/js/game.js
@@ -0,0 +1,22 @@
+// javascript
+
+$(document).ready( function () {
+    let log = function (para) {
+        console.log(para)
+    };
+
+    function getRandomInteger(min, max) {
+        let randomInt = Math.floor((Math.random * (max - min)) + min);
+        return randomInt;
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+
+    log("document ready");
+});
