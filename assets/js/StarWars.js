1a0f5f7712ea4255f7afceacef5603e02b1e120b
JarretYingling <44506154+JarretYingling@users.noreply.github.com>
2018-12-14 00:16:15 -0500

    crystal game completed and partial Star Wars

--- assets/js/StarWars.js
+++ assets/js/StarWars.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         this.name = name;
         this.img_src = "assets/img/png/" + fileName; // html img src file path
         this.id = id; // css id
-        this.classes = classes.join(" "); // css classes
+        this.classes = classes; // css classes
         this.force = force;
         this.hp = hp;
         this.health = health;
@@ -19,8 +19,9 @@ $(document).ready(function () {
         this.updateOpponent = function () {
             this.opponent = true;
         };
-        this.updateClasses = function () {
-            $("#" + this.id).attr("class", this.classes);
+        this.updateClasses = function (newClass) {
+            this.classes.push(newClass);
+            $("#" + this.id).attr("class", this.classes.join(" "));
         };
         this.attack = function () {
 
@@ -28,12 +29,11 @@ $(document).ready(function () {
     };
 
     let game = {
-        /*
+        active: false,
         chooseNextOpponent: false,
         force: null,
         lightSide: [],
         darkside: [],
-        */
         getRandomInt: function (min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
@@ -45,11 +45,10 @@ $(document).ready(function () {
             this.active = false;
             this.chooseNextOpponent = false;
             this.force = null;
-            this.alive = [];
             const LIGHTCLASSES = ["img", "character", "light", "test-light"];
             const DARKCLASSES = ["img", "character", "dark", "test-dark"];
             const LIGHTSIDE = ["light",
-                new Character("Yoda", "yoda.png", "yoda", LIGHTCLASSES, true, 25, 200),
+                new Character("Yoda", "yoda.png", "l1", LIGHTCLASSES, true, 25, 200),
                 new Character("Luke Skywalker", "luke-skywalker.png", "luke", LIGHTCLASSES, true, 25, 200),
                 new Character("Princess Leia", "princess-leia.png", "leia", LIGHTCLASSES, true, 25, 200),
                 new Character("Han Solo", "han-solo.png", "han", LIGHTCLASSES, true, 25, 200),
@@ -59,7 +58,7 @@ $(document).ready(function () {
                 new Character("R2-D2", "r2d2.png", "r2d2", LIGHTCLASSES, true, 25, 200)
             ];
             const DARKSIDE = ["dark",
-                new Character("Emporer Palpatine", "emporer-palpatine.png", "emporer", DARKCLASSES, false, 25, 200),
+                new Character("Emporer Palpatine", "emporer-palpatine.png", "d1", DARKCLASSES, false, 25, 200),
                 new Character("Darth Vader", "darth-vader.png", "vader", DARKCLASSES, false, 25, 200),
                 new Character("Jabba the Hutt", "jabba-the-hutt.png", "jabba", DARKCLASSES, false, 25, 200),
                 new Character("Greedo", "greedo.png", "greedo", DARKCLASSES, false, 25, 200),
@@ -70,8 +69,6 @@ $(document).ready(function () {
             ];
             this.lightSide = LIGHTSIDE;
             this.darkSide = DARKSIDE;
-            this.lightIds = [];
-            this.darkIds = [];
             console.log(this.lightSide);
             console.log(this.darkSide);
             console.log("finish resetGame");
@@ -84,11 +81,11 @@ $(document).ready(function () {
             console.log("finish startGame");
             return;
         },
-        chooseCharacter: function (id) {
-            console.log("start chooseCharacter")
+        chooseCharacter: function (force, index) {
+            console.log("start chooseCharacter");
             // choose opponent
             if (this.nextOpponent) {
-                console.log("updateOpponent")
+                console.log("updateOpponent");
                 // this.opponent = true;
                 this.nextOpponent = false;
                 return;
@@ -98,12 +95,32 @@ $(document).ready(function () {
             }
             // choose player
             console.log("updatePlayer");
-            this.lightSide[1].updatePlayer();
-            this.lightSide[1].updateClasses();
             this.active = true;
+            let others = [];
+            switch (force) {
+                case "l":
+                    this.force = true;
+                    this.lightSide[index].updatePlayer();
+                    /*
+                    this.lightSide.forEach(function() {
+                        this.updateClasses("invisible");
+                    });
+                    */
+                    console.log(this.lightSide);
+                    break;
+                case "d":
+                    this.game.force = false;
+                    this.darkSide[index].updatePlayer();
+                    break;
+                default:
+                    break;    
+            }
             this.nextOpponent = true;
             console.log("finish chooseCharacter");
             return;
+        },
+        updateGame: function () {
+        
         }
     }
 
@@ -113,8 +130,11 @@ $(document).ready(function () {
     // on character image click
     $(".character").on("click", function () {
         let id = $(this).attr("id");
+        // let force = $(this).attr("class").split(" ").pop();
         console.log(id);
-        game.chooseCharacter(id);
+        // console.log(force);
+        game.chooseCharacter(id.charAt(0), id.charAt(1));
+
     });
 
 });
